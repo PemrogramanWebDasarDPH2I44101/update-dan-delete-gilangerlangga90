36ee5f2cc6f7473e5499bb6a4dc129fe189df491
gilangerlangga90 <42725308+gilangerlangga90@users.noreply.github.com>
2018-10-05 20:14:17 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
-$servername = "localhost";
-$username   = "admin";
-$password   = "1234";
-$db         = "WebDasar";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "universitas";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+    //Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
+
+    //Check Connection
+    if (!$conn) {
+        die("Connection failed: ".mysqli_connect_error());
+    }
+   
 ?>
